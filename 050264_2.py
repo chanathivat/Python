1c4a769e7ba522e7ffc72dadc1c8df39f9a60e20
Chanathivat Panadram <chanathivat@gmail.com>
2021-02-05 22:23:37 +0700

    Update

--- 050264_2.py
+++ 050264_2.py
@@ -1,17 +1,18 @@
-A = []
 class shop:
+    A = []
+    B = []
     def __init__(self,name,item,price,amount):
         self.name = name
         self.item = item
+        shop.A.append(item)
         self.price = price
+        shop.B.append(price)
         self.amount = amount
+
     def choice(self):
         global choice
         print('*'*10,'YuRINN','*'*10)
         #choice = input('\tแสดงรายการสินค้า [a]\n\tเพิ่มรายการสินค้า [s]\n\tออกจากระบบ [x]\n\nกรุณาเลือกคำสั่งสินค้า : ')
-        return(
-            ()
-        )
-    def show()
-        
-shop.choice()
+
+
+print shop.A
