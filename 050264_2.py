9d101763b70eba2418371fe0169b9e493d3538c3
Chanathivat Panadram <chanathivat@gmail.com>
2021-02-05 18:20:47 +0700

    Weak1

---
+++ 050264_2.py
@@ -0,0 +1,17 @@
+A = []
+class shop:
+    def __init__(self,name,item,price,amount):
+        self.name = name
+        self.item = item
+        self.price = price
+        self.amount = amount
+    def choice(self):
+        global choice
+        print('*'*10,'YuRINN','*'*10)
+        #choice = input('\tแสดงรายการสินค้า [a]\n\tเพิ่มรายการสินค้า [s]\n\tออกจากระบบ [x]\n\nกรุณาเลือกคำสั่งสินค้า : ')
+        return(
+            ()
+        )
+    def show()
+        
+shop.choice()
