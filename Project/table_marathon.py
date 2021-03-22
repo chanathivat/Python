33fedfdbec50ecbea2e62842fead5c036184c21c
Chanathivat Panadram <chanathivat@gmail.com>
2021-03-22 21:33:29 +0700

    s

--- Project/table_marathon.py
+++ Project/table_marathon.py
@@ -3,14 +3,14 @@ conn = sqlite3.connect(r'C:\Users\User\Desktop\Chanathivat_python\Project\Marath
 c = conn.cursor()
 
 c.execute("""CREATE TABLE marathon (id integer PRIMARY KEY AUTOINCREMENT,
-    fname varchar(30) NOT NULL,
-    lname varchar(30) NOT NULL,
-    ids varchar(20) NOT NULL,
-    sex varchar(10) NOT NULL,
-    age varchar(10) NOT NULL,
-    typerun varchar(30) NOT NULL,
-    email varchar(50) NOT NULL,
-    num varchar(10) NOT NULL)""")
+    fname varchar(30) NULL,
+    lname varchar(30) NULL,
+    ids varchar(20) NULL,
+    sex varchar(10) NULL,
+    age varchar(10) NULL,
+    typerun varchar(30) NULL,
+    email varchar(50) NULL,
+    num varchar(10) NULL)""")
     
 conn.commit()
 conn.close()
